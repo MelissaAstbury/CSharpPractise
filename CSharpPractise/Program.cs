b8a73ce39f56cab376ee15383aec6f57e643f7d6
Melissa Astbury <melissaastbury@hotmail.com>
2021-01-20 22:18:15 +0000

    created two variables for my console story so name and age are stored as changable values

--- CSharpPractise/Program.cs
+++ CSharpPractise/Program.cs
@@ -6,7 +6,16 @@ namespace CSharpPractise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //create a variable (container) to contain data - name and age will change
+            string characterName = "Holly";
+            int characterAge = 10;
+
+            Console.WriteLine("There is a girl called " + characterName);
+            Console.WriteLine("She is " + characterAge + " years old");
+            Console.WriteLine("Her bestfriend is also called Holly");
+            Console.WriteLine("They are both 10 years old");
+
             Console.ReadLine();
         }
     }
